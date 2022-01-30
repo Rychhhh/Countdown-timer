1383a59ce0a47a58fca2b6dc0a9ee6656c2a2532
Han <80162410+Ryhann@users.noreply.github.com>
2022-01-30 22:47:07 +0700

    update year

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const newYears = "1 Jan 2022";
+const newYears = "1 Jan 2023";
 
 function countdown() {
   const newYearDate = new Date(newYears);
